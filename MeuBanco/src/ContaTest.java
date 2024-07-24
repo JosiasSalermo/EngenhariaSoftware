5221fe20d52be235d5f0e3e96ea9e35d33cd8029
Josias <josiassalermojunior>
2024-07-24 11:07:27 -0300

    criando testes

---
+++ MeuBanco/src/ContaTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaTest {
+
+    @Test
+    void depositar() {
+    }
+
+    @Test
+    void sacar() {
+    }
+
+    @Test
+    void getSaldo() {
+    }
+}
